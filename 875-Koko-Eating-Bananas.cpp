5b9dedb93fa429181568893bb3efd87f6b1b6be8
Ayush Kumar <142439633+ayush-309@users.noreply.github.com>
2025-01-21 23:58:31 +0530

    [875] [Time Beats: 14.47%] [Memory Beats: 70.85%] - LeetPush

---
+++ 875-Koko-Eating-Bananas.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int minSpeed = INT_MIN;
+        for (int i = 0; i < piles.size(); i++) {
+            minSpeed = max(minSpeed, piles[i]);
+        }
+        if (h == piles.size()) {
+            return minSpeed;
+        }
+        int low = 1, high = minSpeed;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            long int hours = 0;
+            for (int i = 0; i < piles.size(); i++) {
+                hours += ceil(double(piles[i])/ double(mid));
+                // hours += ceil(piles[i]*1.0/ mid);
+            }
+            if (hours == h) {
+                minSpeed = mid;
+                high = mid-1;
+            } else if (hours > h) {
+                low = mid+1;
+            } else {
+                minSpeed=mid;
+                high = mid-1;
+            }
+        }
+        return minSpeed;
+    }
+};
