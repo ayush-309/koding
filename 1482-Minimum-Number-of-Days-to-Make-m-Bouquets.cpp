7bd2a80123adbec27085150a6acfe104ccd44dfc
Ayush Kumar <142439633+ayush-309@users.noreply.github.com>
2025-01-23 00:25:32 +0530

    [1482] [Time Beats: 64.86%] [Memory Beats: 17.46%] - LeetPush

---
+++ 1482-Minimum-Number-of-Days-to-Make-m-Bouquets.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    long int minDays(vector<int>& bloomDay, long int m, long int k) {
+        if (m*k>bloomDay.size()){
+            return -1;
+        }
+        long int low = INT_MAX, high = INT_MIN;
+        long int ans=INT_MAX;
+        for (long int i=0; i<bloomDay.size(); i++){
+            if (bloomDay[i]<low){
+                low = bloomDay[i];
+            }
+            if (bloomDay[i]>high){
+                high = bloomDay[i];
+            }
+        }
+        while (low<=high){
+            long int mid = (low+high)/2;
+            long int count=0, flag=0;
+            for (long int i=0; i<bloomDay.size(); i++){
+                if (mid>=bloomDay[i]){
+                    flag++;
+                }
+                else{
+                    flag=0;
+                }
+                if (flag==k){
+                    count++;
+                    flag=0;
+                }
+            }
+            cout<<mid<<", "<<count<<endl;
+            if (count>=m){
+                ans = min(ans, mid);
+                high = mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
