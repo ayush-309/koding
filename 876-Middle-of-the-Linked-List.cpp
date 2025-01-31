760b4eba56b1275b37896fc7caebec6c5e7bba28
Ayush Kumar <142439633+ayush-309@users.noreply.github.com>
2025-02-01 00:12:55 +0530

    [876] [Time Beats: 100.00%] [Memory Beats: 25.54%] - LeetPush

--- 876-Middle-of-the-Linked-List.cpp
+++ 876-Middle-of-the-Linked-List.cpp
@@ -10,28 +10,41 @@
  */
 class Solution {
 public:
+    // Mine brute force approach
+
+    // ListNode* middleNode(ListNode* head) {
+    //     if (head->next == NULL){
+    //         return head;
+    //     }
+    //     ListNode *mover = head;
+    //     int count = 1;
+    //     while (mover->next!=NULL){
+    //         count++;
+    //         mover =  mover->next;
+    //     }
+    //     int index = count/2+1;
+    //     count = 1;
+    //     mover = head;
+    //     while (count!=index){
+    //         count++;
+    //         mover = mover->next;
+    //     }
+    //     return mover;
+    // }
+
+    // two-pointer approach (slow and fast pointers)
+
     ListNode* middleNode(ListNode* head) {
-        if (head->next == NULL){
-            return head;
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next;
+            if (fast->next!=NULL){
+                fast = fast->next;
+            }
         }
-        ListNode *mover = head;
-        int count = 1;
-        while (mover->next!=NULL){
-            count++;
-            mover =  mover->next;
-        }
-        int index;
-        if (count%2==0){
-            index = count/2+1;
-        } else {
-            index = (count+1)/2;
-        }
-        count = 1;
-        mover = head;
-        while (count!=index){
-            count++;
-            mover = mover->next;
-        }
-        return mover;
+        return slow;
     }
 };
