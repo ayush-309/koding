a36aa1c0c690cf0b2ea90e2123fcf84e89641b17
Ayush Kumar <142439633+ayush-309@users.noreply.github.com>
2025-02-12 13:22:24 +0530

    [2342] [Time Beats: 90.25%] [Memory Beats: 95.95%] - LeetPush

--- 2342-Max-Sum-of-a-Pair-With-Equal-Sum-of-Digits.cpp
+++ 2342-Max-Sum-of-a-Pair-With-Equal-Sum-of-Digits.cpp
@@ -12,19 +12,34 @@ public:
     }
 
     int maximumSum(vector<int>& nums) {
-        int maxSum = -1;
-        int sum, dSum;
-        map<int, int> mpp;
-        for (int i = 0; i < nums.size(); i++) {
-            dSum = sumOfDigits(nums[i]);
-            if (mpp.find(dSum) != mpp.end()) {
-                sum = nums[i] + nums[mpp[dSum]];
-                maxSum = max(sum, maxSum);
-                mpp[dSum] = nums[i] > nums[mpp[dSum]] ? i : mpp[dSum];
-            } else {
-                mpp[dSum] = i;
-            }
+        // int maxSum = -1;
+        // int sum, dSum;
+        // map<int, int> mpp;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     dSum = sumOfDigits(nums[i]);
+        //     if (mpp.find(dSum) != mpp.end()) {
+        //         sum = nums[i] + nums[mpp[dSum]];
+        //         maxSum = max(sum, maxSum);
+        //         mpp[dSum] = nums[i] > nums[mpp[dSum]] ? i : mpp[dSum];
+        //     } else {
+        //         mpp[dSum] = i;
+        //     }
+        // }
+        // return maxSum;
+
+        int mp[82];  // Stores max num for each digit sum
+        memset(mp, -1, sizeof(mp));
+        int ans = -1;
+
+        for (int num : nums) {
+            int sumDigits = 0, temp = num;
+            while (temp) sumDigits += temp % 10, temp /= 10;
+
+            if (mp[sumDigits] != -1)
+                ans = max(ans, num + mp[sumDigits]);
+
+            mp[sumDigits] = max(mp[sumDigits], num);
         }
-        return maxSum;
+        return ans;
     }
 };
