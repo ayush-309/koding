be4261fded3c81df66fd7f7219dd785c5b01aa97
Ayush Kumar <142439633+ayush-309@users.noreply.github.com>
2025-03-06 12:02:46 +0530

    [2579] [Time Beats: 100.00%] [Memory Beats: 30.59%] - LeetPush

--- Count-Total-Number-of-Colored-Cells.cpp
+++ Count-Total-Number-of-Colored-Cells.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long ans = 1;
-        for (int i=0; i<n; i++){
-            ans += 4*i;
-        }
-        return ans;
+        
+        return (long long)((pow(n,2)+pow(n-1,2)));
     }
 };
