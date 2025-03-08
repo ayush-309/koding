cad5b8948cdf6bc9adf55c620c5e1532a878f1e2
Ayush Kumar <142439633+ayush-309@users.noreply.github.com>
2025-03-08 15:39:21 +0530

    [225] [Time Beats: 100.00%] [Memory Beats: 85.48%] - LeetPush

--- Implement-Stack-using-Queues.cpp
+++ Implement-Stack-using-Queues.cpp
@@ -1,51 +1,81 @@
 class MyStack {
 public:
-    queue<int> q1, q2;
+    queue<int> q;
+
     MyStack() {}
-
+    
     void push(int x) {
-        if (q1.empty())
-            q1.push(x);
-        else {
-            q1.pop();
-            q1.push(x);
-        }
-        q2.push(x);
+        q.push(x);
     }
-
+    
     int pop() {
-        q1.pop();
-        int x;
-        while (true) {
-            x = q2.front();
-            q2.pop();
-            if (q2.empty())
-                break;
-            q1.push(x);
+        int n = q.size();
+        for (int i=0; i<n-1; i++){
+            q.push(q.front());
+            q.pop();
         }
-        if (!q1.empty()) {
-            q2 = q1;
-            int y;
-            while (true) {
-                y = q1.front();
-                q1.pop();
-                if (q1.empty()) {
-                    q1.push(y);
-                    break;
-                }
-            }
-        }
+        int x = q.front();
+        q.pop();
         return x;
     }
-
-    int top() { return q1.front(); }
-
+    
+    int top() {
+        return q.back();
+    }
+    
     bool empty() {
-        if (q2.empty())
-            return true;
-        return false;
+        return q.empty();
     }
 };
+
+// class MyStack {
+// public:
+//     queue<int> q1, q2;
+//     MyStack() {}
+
+//     void push(int x) {
+//         if (q1.empty())
+//             q1.push(x);
+//         else {
+//             q1.pop();
+//             q1.push(x);
+//         }
+//         q2.push(x);
+//     }
+
+//     int pop() {
+//         q1.pop();
+//         int x;
+//         while (true) {
+//             x = q2.front();
+//             q2.pop();
+//             if (q2.empty())
+//                 break;
+//             q1.push(x);
+//         }
+//         if (!q1.empty()) {
+//             q2 = q1;
+//             int y;
+//             while (true) {
+//                 y = q1.front();
+//                 q1.pop();
+//                 if (q1.empty()) {
+//                     q1.push(y);
+//                     break;
+//                 }
+//             }
+//         }
+//         return x;
+//     }
+
+//     int top() { return q1.front(); }
+
+//     bool empty() {
+//         if (q2.empty())
+//             return true;
+//         return false;
+//     }
+// };
 
 /**
  * Your MyStack object will be instantiated and called as such:
