1a632a41ce238d71c3ebb0c93919567c546fc0e2
Ayush Kumar <142439633+ayush-309@users.noreply.github.com>
2025-03-17 15:13:20 +0530

    [2206] [Time Beats: 30.21%] [Memory Beats: 31.79%] - LeetPush

---
+++ Divide-Array-Into-Equal-Pairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        map<int, int> mpp;
+        for (int i=0; i<nums.size(); i++){
+            mpp[nums[i]]++;
+        }
+        for (auto it:mpp){
+            if (it.second%2==1) return false; 
+        }
+        return true;
+    }
+};
