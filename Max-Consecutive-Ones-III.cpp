a091338c82635732e54a6433d2a8e32a03ee6e6a
Ayush Kumar <142439633+ayush-309@users.noreply.github.com>
2025-05-24 13:07:19 +0530

    [1004] [Time Beats: 100.00%] [Memory Beats: 91.09%] - LeetPush

--- Max-Consecutive-Ones-III.cpp
+++ Max-Consecutive-Ones-III.cpp
@@ -1,38 +1,17 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        // int i = 0, j;
-        // for (j = 0; j < nums.size(); ++j) {
-        //     if (nums[j] == 0)
-        //         k--; // Use a flip
-        //     if (k < 0) {
-        //         if (nums[i] == 0)
-        //             k++; // Reclaim the flip
-        //         i++;
-        //     }
-        // }
-        // return j - i; // Window size
-
-        int i = 0, maxLen = 0;
-        int zeroCount = 0;
-
-        for (int j = 0; j < nums.size(); ++j) {
-            if (nums[j] == 0) {
-                zeroCount++; // We are flipping this 0
+        int maxLen=0;
+        int i=0, j=0, zeroes=0, n=nums.size();
+        while (j<n){
+            if (nums[j]==0) zeroes++;
+            if (zeroes>k){
+                if (nums[i]==0) zeroes--;
+                i++;
             }
-
-            // If we flipped more than k zeros, move the left pointer
-            while (zeroCount > k) {
-                if (nums[i] == 0) {
-                    zeroCount--; // Reclaim this flip
-                }
-                i++; // Shrink the window from the left
-            }
-
-            // Update the maximum window length
-            maxLen = max(maxLen, j - i + 1);
+            if (zeroes<=k) maxLen = max(maxLen, j-i+1);
+            j++;
         }
-
         return maxLen;
     }
 };
