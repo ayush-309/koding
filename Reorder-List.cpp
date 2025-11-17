9ca27538ef4a631bedce17f077b7c93c3ed3a597
Ayush Kumar <142439633+ayush-309@users.noreply.github.com>
2025-11-17 19:27:15 +0530

    [143] [Time Beats: 100.00%] [Memory Beats: 80.20%] - LeetPush

--- Reorder-List.cpp
+++ Reorder-List.cpp
@@ -10,28 +10,59 @@
  */
 class Solution {
 public:
+    ListNode* reverseList(ListNode *head){
+        ListNode *prev=NULL, *curr=head, *aage=head;
+        while (curr != NULL){
+            aage = aage->next;
+            curr->next = prev;
+            prev = curr;
+            curr = aage;
+        }
+        return prev;
+    }
+
     void reorderList(ListNode* head) {
-        if (head->next == NULL || head->next->next == NULL) return;
+        ListNode *slow = head, *fast = head;
 
-        vector<ListNode *> store; 
-        ListNode *mover = head;
-
-        while (mover!=NULL){
-            store.push_back(mover);
-            mover = mover->next;
+        while (fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        int n = store.size()-1;
-        int i=1;
+        ListNode* part = reverseList(slow->next);
+        slow->next = NULL;
+        ListNode* mover = head;
 
-        mover = head;
-        while (i<=n){
-            mover->next = store[n--];
-            mover = mover->next;
-            if (n<i) break;
-            mover->next = store[i++];
-            mover = mover->next;
+        while (mover != NULL && part != NULL){
+            slow = mover->next;
+            mover->next = part;
+            mover = slow;
+            fast = part->next;
+            part->next = slow;
+            part = fast;
         }
-        mover->next = NULL;
+
+        // if (head->next == NULL || head->next->next == NULL) return;
+
+        // vector<ListNode *> store; 
+        // ListNode *mover = head;
+
+        // while (mover!=NULL){
+        //     store.push_back(mover);
+        //     mover = mover->next;
+        // }
+
+        // int n = store.size()-1;
+        // int i=1;
+
+        // mover = head;
+        // while (i<=n){
+        //     mover->next = store[n--];
+        //     mover = mover->next;
+        //     if (n<i) break;
+        //     mover->next = store[i++];
+        //     mover = mover->next;
+        // }
+        // mover->next = NULL;
     }
 };
